0dea0b0307fe7a5f019f9cc4923aa487cfbdfae8
FiveMovesAhead <ying@tig.foundation>
2024-08-28 19:42:38 +0800

    Player 0x18f8955ec60604fb8187c27732afe4d4a3fc4e26 submitted 'faster_walk_sat' for challenge satisfiability

--- tig-benchmarker/src/benchmarker/cuda_run_benchmark.rs
+++ tig-benchmarker/src/benchmarker/cuda_run_benchmark.rs
@@ -132,8 +132,8 @@ pub async fn execute(
                                     // #[cfg(feature = "c001_a012")]
                                     // "c001_a012" => Some((c001::c001_a012::cuda_solve_challenge as CudaSolveChallengeFn, &c001::c001_a012::KERNEL)),
 
-                                    // #[cfg(feature = "c001_a013")]
-                                    // "c001_a013" => Some((c001::c001_a013::cuda_solve_challenge as CudaSolveChallengeFn, &c001::c001_a013::KERNEL)),
+                                    #[cfg(feature = "c001_a013")]
+                                    "c001_a013" => Some((c001::c001_a013::cuda_solve_challenge as CudaSolveChallengeFn, &c001::c001_a013::KERNEL)),
 
                                     // #[cfg(feature = "c001_a014")]
                                     // "c001_a014" => Some((c001::c001_a014::cuda_solve_challenge as CudaSolveChallengeFn, &c001::c001_a014::KERNEL)),
