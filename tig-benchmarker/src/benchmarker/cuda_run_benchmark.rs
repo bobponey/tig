8d65c3ccb63c6ce23689d1a36d031552b0ea1c4d
FiveMovesAhead <ying@tig.foundation>
2024-08-28 19:43:07 +0800

    Player 0x51df7b838a61d95b3f7508ac84316f4daf656302 submitted 'double_optimized' for challenge satisfiability

--- tig-benchmarker/src/benchmarker/cuda_run_benchmark.rs
+++ tig-benchmarker/src/benchmarker/cuda_run_benchmark.rs
@@ -150,8 +150,8 @@ pub async fn execute(
                                     // #[cfg(feature = "c001_a018")]
                                     // "c001_a018" => Some((c001::c001_a018::cuda_solve_challenge as CudaSolveChallengeFn, &c001::c001_a018::KERNEL)),
 
-                                    // #[cfg(feature = "c001_a019")]
-                                    // "c001_a019" => Some((c001::c001_a019::cuda_solve_challenge as CudaSolveChallengeFn, &c001::c001_a019::KERNEL)),
+                                    #[cfg(feature = "c001_a019")]
+                                    "c001_a019" => Some((c001::c001_a019::cuda_solve_challenge as CudaSolveChallengeFn, &c001::c001_a019::KERNEL)),
 
                                     // #[cfg(feature = "c001_a020")]
                                     // "c001_a020" => Some((c001::c001_a020::cuda_solve_challenge as CudaSolveChallengeFn, &c001::c001_a020::KERNEL)),
--- tig-benchmarker/src/benchmarker/run_benchmark.rs
+++ tig-benchmarker/src/benchmarker/run_benchmark.rs
@@ -95,8 +95,8 @@ pub async fn execute(
                                     // #[cfg(feature = "c001_a018")]
                                     // "c001_a018" => Some(c001::c001_a018::solve_challenge as SolveChallengeFn),
 
-                                    // #[cfg(feature = "c001_a019")]
-                                    // "c001_a019" => Some(c001::c001_a019::solve_challenge as SolveChallengeFn),
+                                    #[cfg(feature = "c001_a019")]
+                                    "c001_a019" => Some(c001::c001_a019::solve_challenge as SolveChallengeFn),
 
                                     // #[cfg(feature = "c001_a020")]
                                     // "c001_a020" => Some(c001::c001_a020::solve_challenge as SolveChallengeFn),
