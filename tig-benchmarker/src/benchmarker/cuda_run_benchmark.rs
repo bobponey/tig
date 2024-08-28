96c2b55ba560b921ba134408a3476a1e22e63b81
FiveMovesAhead <ying@tig.foundation>
2024-08-28 19:47:30 +0800

    Player 0x9e1a232ded4d9ee9f1eb78f146cb13ee6c84c953 submitted 'tinhat_pete' for challenge knapsack

--- tig-benchmarker/src/benchmarker/cuda_run_benchmark.rs
+++ tig-benchmarker/src/benchmarker/cuda_run_benchmark.rs
@@ -6230,8 +6230,8 @@ pub async fn execute(
                                     // #[cfg(feature = "c003_a008")]
                                     // "c003_a008" => Some((c003::c003_a008::cuda_solve_challenge as CudaSolveChallengeFn, &c003::c003_a008::KERNEL)),
 
-                                    // #[cfg(feature = "c003_a009")]
-                                    // "c003_a009" => Some((c003::c003_a009::cuda_solve_challenge as CudaSolveChallengeFn, &c003::c003_a009::KERNEL)),
+                                    #[cfg(feature = "c003_a009")]
+                                    "c003_a009" => Some((c003::c003_a009::cuda_solve_challenge as CudaSolveChallengeFn, &c003::c003_a009::KERNEL)),
 
                                     // #[cfg(feature = "c003_a010")]
                                     // "c003_a010" => Some((c003::c003_a010::cuda_solve_challenge as CudaSolveChallengeFn, &c003::c003_a010::KERNEL)),
